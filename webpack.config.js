bef8f1b2db1105ac764a23585a8fc109870a4133
lan <taosha.1219@bytedance.com>
2022-11-22 10:53:18 +0800

    feat: 部署GitHub page

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     mode: 'development',
     output: {
         filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'docs'),
         clean: true,
         publicPath: '/',
     },
