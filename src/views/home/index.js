69203f043ab5c463217e38cc01d8df729ed9f460
lan <taosha.1219@bytedance.com>
2023-01-02 13:49:13 +0800

    feat: 搜索完成

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,15 +1,45 @@
 import $ from 'jquery';
 import card from '../../components/card.art';
+import loading from '../../components/loading.art';
 
 export const homeInit = () => {
     // 获取top50首歌曲
     $.ajax({
         url: 'https://netease-cloud-music-api-tan-xi.vercel.app/artist/top/song?id=980025',
         success: (res) => {
-            const songs = res.songs;
+            const songs = res.songs.map((item) => {
+                return {
+                    name: item.name,
+                    alName: item.al.name,
+                };
+            });
             $('#music-list').append(card({ songs }));
         },
     });
+
+    $('#search-submit').click(() => {
+        const key = $('#search-key').val();
+        $('#music-list').empty();
+        $('#music-list').append(loading);
+        $.ajax({
+            url: 'https://netease-cloud-music-api-tan-xi.vercel.app/search?keywords=周兴哲 ' + key,
+            success: (res) => {
+                console.log(res);
+                // 筛选出周兴哲演唱歌曲
+                const songs = res.result.songs
+                    .filter((item) => item.artists[0].id == 980025)
+                    .map((item) => {
+                        return {
+                            name: item.name,
+                            alName: item.album.name,
+                        };
+                    });
+                $('#music-list').empty();
+                $('#music-list').append(card({ songs }));
+            },
+        });
+    });
+
     $('.more-icon').click(() => {
         window.location.hash = '#/about';
     });
